e58193a58d4362f717c6a867684f8b5b04bb458e
TAPgiles <55659175+TAPgiles@users.noreply.github.com>
2019-12-08 12:49:36 +0000

    Create AND.js

---
+++ AND.js
@@ -0,0 +1,19 @@
+function AND(number_of_ports) {
+	this.number_of_ports = number_of_ports;
+}
+Object.defineProperties(AND.prototype, {
+	A: {
+		set: function(value) { // input signal
+			this.A_current = value;
+		}
+	}, // repeat for all ports
+
+	result: {
+		get: function() { // output signal
+			var result = this.A_current;
+			if ((this.number_of_ports >= 3) && (this.B_current < result)) {
+				result = this.B_current;
+			} // repeat for each port
+		}
+	}
+});
